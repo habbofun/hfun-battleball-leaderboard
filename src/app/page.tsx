2371bce1c601c9596fef17026a14926c5522983c
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-08-25 07:17:45 +0200

    Added website link to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,12 +132,17 @@ export default function Leaderboard() {
                         HFUN Leaderboard
                     </TextEffect>
                     <div className="flex justify-end items-center gap-2">
-                        <ModeToggle />
-                        <Button variant="outline">
+                        <Button variant="ghost">
+                            <Link href="https://habbofun.org" target="_blank" rel="noopener noreferrer">
+                                Website
+                            </Link>
+                        </Button>
+                        <Button variant="ghost">
                             <Link href="https://discord.gg/originses" target="_blank" rel="noopener noreferrer">
                                 Discord
                             </Link>
                         </Button>
+                        <ModeToggle />
                     </div>
                 </div>
             </header>
