564a0ec2553fb5781c102bc1a9db86a8f430365c
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-08-25 05:36:52 +0200

    Added toaster to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,34 +3,36 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
+    subsets: ["latin"],
+    variable: "--font-sans",
 });
 
 export const metadata: Metadata = {
-  title: "hfun.info",
-  description: "Habbo Origins: ES | Battleball Leaderboard",
+    title: "hfun.info",
+    description: "Habbo Origins: ES | Battleball Leaderboard",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={cn(fontSans.variable, "font-sans")}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={cn(fontSans.variable, "font-sans")}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+                <Toaster />
+            </body>
+        </html>
+    );
 }
