94d1639da0c7f211ef51669d4fd1df5edf824202
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-02 18:15:43 +0200

    Made the routes use the ratelimiter middleware

--- src/app/api/healthcheck/route.ts
+++ src/app/api/healthcheck/route.ts
@@ -1,5 +1,11 @@
+import { withRateLimit } from "@/lib/ratelimit-middleware";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
-    return NextResponse.json({ status: "ok" });
+async function healthcheckHandler(req: NextRequest) {
+    return NextResponse.json(
+        { status: "ok", message: "https://hfun.info" },
+        { status: 200 }
+    );
 }
+
+export const GET = (req: NextRequest) => withRateLimit(req, healthcheckHandler);
