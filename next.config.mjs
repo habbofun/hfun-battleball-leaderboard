fa41ad740df84feed45a36fcc8e2d486ce901f77
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-02 18:41:50 +0200

    Added habbo for image providers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "www.habbo.es",
+                pathname: "/habbo-imaging/**",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
