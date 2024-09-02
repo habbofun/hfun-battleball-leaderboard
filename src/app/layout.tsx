274b9661d6bf4a38a0a050e352f8ed739387c477
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-02 23:35:49 +0200

    Add page footer to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
-import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Providers } from "@/components/providers";
 import { PageHeader } from "@/components/page-header";
+import { PageFooter } from "@/components/page-footer";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
     subsets: ["latin"],
     variable: "--font-sans",
 });
 
-export function generateMetadata(): Metadata {
-    return {
-        title: "hfun.info",
-        description: "Habbo Origins: ES | Fansite",
-    };
-}
+export const metadata: Metadata = {
+    title: "hfun.info",
+    description: "Habbo Origins: ES | Fansite",
+};
 
 export default function RootLayout({
     children,
@@ -25,12 +24,17 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <body
-                className={cn(fontSans.variable, "font-sans")}
+                className={cn(
+                    fontSans.variable,
+                    "font-sans",
+                    "flex flex-col min-h-screen"
+                )}
                 suppressHydrationWarning
             >
                 <Providers>
                     <PageHeader />
-                    {children}
+                    <main className="flex-grow">{children}</main>
+                    <PageFooter />
                 </Providers>
             </body>
         </html>
