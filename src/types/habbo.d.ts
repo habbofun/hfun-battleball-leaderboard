d30c51a9f17367f3def5678fa0c4449854540231
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-03 16:51:47 +0200

    Change file name to follow conventions

---
+++ src/types/habbo.d.ts
@@ -0,0 +1,39 @@
+export interface HabboUserInfo {
+  uniqueId: string;
+  name: string;
+  figureString: string;
+  motto: string;
+  online: boolean;
+  lastAccessTime: string;
+  memberSince: string;
+  profileVisible: boolean;
+  currentLevel: number;
+  currentLevelCompletePercent: number;
+  totalExperience: number;
+  starGemCount: number;
+  selectedBadges: Badge[];
+  bouncerPlayerId: string;
+}
+
+interface Badge {
+  badgeIndex: number;
+  code: string;
+  name: string;
+  description: string;
+}
+
+export interface HabboErrorResponse {
+  error: string;
+}
+
+export interface CatalogItem {
+  name: string;
+  description: string;
+  price: number;
+  itemImageUrl?: string;
+  currencyImageUrl?: string;
+}
+
+export type CatalogData = {
+  [key: string]: CatalogItem[];
+};
