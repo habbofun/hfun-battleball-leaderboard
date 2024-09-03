9c11a390398c60c5f1f8510502ba86cf4590dfea
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-03 03:06:32 +0200

    Added .mjs env vars

---
+++ env.mjs
@@ -0,0 +1,27 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+  /*
+   * Serverside Environment variables, not available on the client.
+   * Will throw if you access these variables on the client.
+   */
+  server: {
+    DATABASE_URL: z.string().url(),
+    AUTH_SECRET: z.string().min(1),
+    AUTH_RESEND_KEY: z.string().min(1),
+    AUTH_URL: z.string().url(),
+  },
+  /*
+   * Due to how Next.js bundles environment variables on Edge and Client,
+   * we need to manually destructure them to make sure all are included in bundle.
+   *
+   * 💡 You'll get type errors if not all variables from `server` & `client` are included here.
+   */
+  runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_RESEND_KEY: process.env.AUTH_RESEND_KEY,
+    AUTH_URL: process.env.AUTH_URL,
+  },
+});
