508c0a7b560f6b081a1c81e4e86e13842358c0e1
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-05 21:46:42 +0200

    Added http error enum instead of hardcoding return statuses

--- src/app/api/habbo/user-info/route.ts
+++ src/app/api/habbo/user-info/route.ts
@@ -2,25 +2,42 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import { HabboErrorResponse, HabboUserInfo } from '@/types/habbo';
 
+enum HttpStatus {
+  BAD_REQUEST = 400,
+  NOT_FOUND = 404,
+  INTERNAL_SERVER_ERROR = 500,
+}
+
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const username = searchParams.get('username');
 
   if (!username) {
-    return NextResponse.json({ error: 'Username is required' }, { status: 400 });
+    return NextResponse.json(
+      { error: 'Username is required' },
+      { status: HttpStatus.BAD_REQUEST },
+    );
   }
 
   try {
-    const response = await fetch(`https://origins.habbo.es/api/public/users?name=${username}`);
+    const response = await fetch(
+      `https://origins.habbo.es/api/public/users?name=${username}`,
+    );
     const data: HabboUserInfo | HabboErrorResponse = await response.json();
 
     if ('error' in data) {
-      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+      return NextResponse.json(
+        { error: 'User not found' },
+        { status: HttpStatus.NOT_FOUND },
+      );
     }
 
     return NextResponse.json(data);
   } catch (error) {
     console.error('Error fetching user info:', error);
-    return NextResponse.json({ error: 'Failed to fetch user info' }, { status: 500 });
+    return NextResponse.json(
+      { error: 'Failed to fetch user info' },
+      { status: HttpStatus.INTERNAL_SERVER_ERROR },
+    );
   }
 }
