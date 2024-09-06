02644f0a7b20bf9af660b8a950a15545b0e5a4ba
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-06 21:35:06 +0200

    Added per category search bar in  the catalog page

--- src/components/habbo/catalog/catalog-tabs.tsx
+++ src/components/habbo/catalog/catalog-tabs.tsx
@@ -1,10 +1,12 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { CatalogCategory } from '@/components/habbo/catalog/catalog-category';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import { CatalogData } from '@/types/habbo';
+import { CatalogData, CatalogItem } from '@/types/habbo';
 
 interface CatalogTabsProps {
   catalogData: CatalogData;
@@ -12,25 +14,49 @@ interface CatalogTabsProps {
 
 export function CatalogTabs({ catalogData }: CatalogTabsProps) {
   const [selectedCategory, setSelectedCategory] = useState('Rares');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredItems, setFilteredItems] = useState<CatalogItem[]>([]);
+
+  useEffect(() => {
+    const items = catalogData[selectedCategory];
+    const filtered = items.filter((item) =>
+      item.name.toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setFilteredItems(filtered);
+  }, [selectedCategory, searchTerm, catalogData]);
 
   return (
-    <Tabs
-      defaultValue="Rares"
-      className="w-full"
-      onValueChange={setSelectedCategory}
-    >
-      <TabsList className="grid w-full grid-cols-4">
-        {Object.keys(catalogData).map((category) => (
-          <TabsTrigger key={category} value={category}>
-            {category}
-          </TabsTrigger>
+    <div className="space-y-4">
+      <div className="py-4">
+        <Label htmlFor="item-filter" className="block mb-2">
+          Search
+        </Label>
+        <Input
+          id="item-filter"
+          placeholder="Enter item name to filter in this category"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          className="max-w-sm"
+        />
+      </div>
+      <Tabs
+        defaultValue="Rares"
+        className="w-full"
+        onValueChange={setSelectedCategory}
+      >
+        <TabsList className="grid w-full grid-cols-4">
+          {Object.keys(catalogData).map((category) => (
+            <TabsTrigger key={category} value={category}>
+              {category}
+            </TabsTrigger>
+          ))}
+        </TabsList>
+        {Object.entries(catalogData).map(([category, items]) => (
+          <TabsContent key={category} value={category} className="mt-4">
+            <CatalogCategory category={category} items={filteredItems} />
+          </TabsContent>
         ))}
-      </TabsList>
-      {Object.entries(catalogData).map(([category, items]) => (
-        <TabsContent key={category} value={category} className="mt-4">
-          <CatalogCategory category={category} items={items} />
-        </TabsContent>
-      ))}
-    </Tabs>
+      </Tabs>
+    </div>
   );
 }
