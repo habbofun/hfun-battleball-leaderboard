ae916e7dee9d5aa6afa111f4b45f8fbc5db4570f
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-10 03:37:05 +0200

    Using logout server action

--- src/components/auth/logout/logout-button.tsx
+++ src/components/auth/logout/logout-button.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { signOut } from 'next-auth/react';
+import { logoutAction } from '@/server/actions/auth/logout/logout-action';
 
 interface LogoutButtonProps {
   children?: React.ReactNode;
@@ -8,7 +8,7 @@ interface LogoutButtonProps {
 
 export const LogoutButton = ({ children }: LogoutButtonProps) => {
   const handleLogout = async () => {
-    await signOut({ callbackUrl: '/login' });
+    await logoutAction();
   };
 
   return (
