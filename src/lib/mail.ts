1cb5047d956f28ed7a694d0a766a43ce59c80f38
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-10 04:05:32 +0200

    Updated emails to match format and always send a raw link to prevent href fails if any mail provider doesn't display it properly or anything

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -14,7 +14,7 @@ export const sendEmailVerification = async (email: string, token: string) => {
         <h1>Verify Your Email</h1>
         <p>Thank you for signing up with HFUN.INFO. To complete your registration/verification, please verify your email address by clicking the link below:</p>
         <p><a href="${verificationLink}">Verify Email</a></p>
-        <p>If the link doesn't work, you can copy and paste the following URL into your browser:</p>
+        <p>If the button above doesn't work, copy and paste the following link into your browser:</p>
         <p>${verificationLink}</p>
         <p>If you didn't request this verification, please ignore this email.</p>
         <p>Best regards,<br>The HFUN.INFO Team</p>
@@ -43,6 +43,8 @@ export const sendPasswordResetEmail = async (email: string, token: string) => {
         <h1>Reset Your Password</h1>
         <p>You have requested to reset your password. Click the link below to set a new password:</p>
         <p><a href="${resetLink}">Reset Password</a></p>
+        <p>If the button above doesn't work, copy and paste the following link into your browser:</p>
+        <p>${resetLink}</p>
         <p>If you didn't request this, please ignore this email.</p>
         <p>This link will expire in 1 hour.</p>
         <p>Best regards,<br>The HFUN.INFO Team</p>
@@ -67,7 +69,11 @@ export const sendTwoFactorEmail = async (email: string, token: string) => {
       subject: 'Two-Factor Authentication - HFUN.INFO',
       html: `
         <h1>Two-Factor Authentication</h1>
-        <p>Your two-factor authentication code is: ${token}</p>
+        <p>Your two-factor authentication code is:</p>
+        <h2>${token}</h2>
+        <p>Please enter this code in the application to complete your login.</p>
+        <p>If you didn't request this code, please ignore this email and secure your account.</p>
+        <p>Best regards,<br>The HFUN.INFO Team</p>
       `,
     });
 
