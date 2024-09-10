c30c009a832a6e0d114ec12fc0d967076f331b25
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-10 02:56:56 +0200

    Moved session provider to core layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
 import { Metadata } from 'next';
+import { SessionProvider } from 'next-auth/react';
 import { Inter as FontSans } from 'next/font/google';
 
 import { Providers } from '@/components/providers/providers';
 import { PageFooter } from '@/components/static/page-footer';
 import { PageHeader } from '@/components/static/page-header';
 import { cn } from '@/lib/utils';
+import { auth } from '@/server/auth';
 
 import './globals.css';
 
@@ -18,27 +20,31 @@ export const metadata: Metadata = {
   description: 'Habbo Origins: ES | Fansite',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={cn(
-          fontSans.variable,
-          'font-sans',
-          'flex flex-col min-h-screen overscroll-none',
-        )}
-        suppressHydrationWarning
-      >
-        <Providers>
-          <PageHeader />
-          <main className="flex-grow">{children}</main>
-          <PageFooter />
-        </Providers>
-      </body>
-    </html>
+    <SessionProvider session={session}>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={cn(
+            fontSans.variable,
+            'font-sans',
+            'flex flex-col min-h-screen overscroll-none',
+          )}
+          suppressHydrationWarning
+        >
+          <Providers>
+            <PageHeader />
+            <main className="flex-grow">{children}</main>
+            <PageFooter />
+          </Providers>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -1,7 +1,5 @@
 import { ReactNode } from 'react';
 
-import { SessionProvider } from 'next-auth/react';
-
 import { QueryProvider } from '@/components/providers/query-provider';
 import { ThemeProvider } from '@/components/providers/theme-provider';
 import { Toaster } from '@/components/ui/sonner';
@@ -10,18 +8,16 @@ import { StarfieldWrapper } from '@/components/ui/starfield-wrapper';
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <QueryProvider>
-      <SessionProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <Toaster />
-        </ThemeProvider>
-        <StarfieldWrapper />
-      </SessionProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+        <Toaster />
+      </ThemeProvider>
+      <StarfieldWrapper />
     </QueryProvider>
   );
 }
