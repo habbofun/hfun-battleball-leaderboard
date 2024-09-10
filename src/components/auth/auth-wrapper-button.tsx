4de49e8b31405c18ed11a2f2cb1d2c410f90f3b7
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-10 02:32:18 +0200

    Removed things while i don't have a wrapper

--- src/components/auth/auth-wrapper-button.tsx
+++ src/components/auth/auth-wrapper-button.tsx
@@ -2,33 +2,23 @@ import Link from 'next/link';
 
 import { LogIn, UserPlus } from 'lucide-react';
 
-import { LogoutButton } from '@/components/auth/logout/logout-button';
 import { Button } from '@/components/ui/button';
-import { auth } from '@/server/auth';
 
-export async function AuthWrapperButton() {
-  const session = await auth();
-
+export function AuthWrapperButton() {
   return (
     <div className="flex items-center space-x-2">
-      {session ? (
-        <LogoutButton />
-      ) : (
-        <>
-          <Link href="/login" passHref>
-            <Button variant="ghost" size="sm">
-              <LogIn className="h-4 w-4 mr-2" />
-              Login
-            </Button>
-          </Link>
-          <Link href="/register" passHref>
-            <Button variant="ghost" size="sm">
-              <UserPlus className="h-4 w-4 mr-2" />
-              Register
-            </Button>
-          </Link>
-        </>
-      )}
+      <Link href="/login" passHref>
+        <Button variant="ghost" size="sm">
+          <LogIn className="h-4 w-4 mr-2" />
+          Login
+        </Button>
+      </Link>
+      <Link href="/register" passHref>
+        <Button variant="ghost" size="sm">
+          <UserPlus className="h-4 w-4 mr-2" />
+          Register
+        </Button>
+      </Link>
     </div>
   );
 }
