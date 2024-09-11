d3d4b7f1006f80f4e31da89d835e9f6f422c082a
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-11 20:14:42 +0200

    Fix finder scroll layout

--- src/app/(hfun)/finder/page.tsx
+++ src/app/(hfun)/finder/page.tsx
@@ -2,7 +2,7 @@ import { HabboUserInfo } from '@/components/habbo/finder/habbo-user-info';
 
 export default function FinderPage() {
   return (
-    <div className="flex flex-col min-h-screen bg-background text-foreground">
+    <div className="flex flex-col flex-1 bg-background text-foreground">
       <main className="flex-grow flex flex-col justify-center items-center p-4">
         <HabboUserInfo />
       </main>
