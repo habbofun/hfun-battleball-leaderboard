40c4a3c8a4b117b44fda4befe3d957ae3930ad35
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-11 05:55:11 +0200

    Add lucia

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@node-rs/argon2'],
+  },
   images: {
     remotePatterns: [
       {
