82141120449307ed8bd442e43b213057db56ad29
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-11 07:33:53 +0200

    Error handling on logout action

--- src/server/actions/auth/logout/logout.ts
+++ src/server/actions/auth/logout/logout.ts
@@ -8,33 +8,42 @@ import { lucia } from '@/server/lucia';
 import { validateRequest } from '@/server/validate';
 
 export async function logout() {
-  'use server';
-  const { session } = await validateRequest();
-  if (!session) {
-    return {
-      error: 'Unauthorized',
-    };
-  }
+  try {
+    const { session } = await validateRequest();
 
-  const dbSession = await db.session.deleteMany({
-    where: {
-      userId: session.userId,
-    },
-  });
+    if (!session) {
+      return {
+        error: 'Unauthorized',
+      };
+    }
 
-  if (!dbSession) {
+    const dbSession = await db.session.deleteMany({
+      where: {
+        userId: session.userId,
+      },
+    });
+
+    if (!dbSession) {
+      return {
+        error: 'Failed to logout',
+      };
+    }
+
+    await lucia.invalidateSession(session.id);
+
+    const sessionCookie = lucia.createBlankSessionCookie();
+    cookies().set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes,
+    );
+
+    return redirect('/');
+  } catch (error) {
+    console.log('Error: ', error);
     return {
+      success: false,
       error: 'Failed to logout',
     };
   }
-
-  await lucia.invalidateSession(session.id);
-
-  const sessionCookie = lucia.createBlankSessionCookie();
-  cookies().set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes,
-  );
-  return redirect('/');
 }
