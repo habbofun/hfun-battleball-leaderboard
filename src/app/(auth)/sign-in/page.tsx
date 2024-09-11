5aeb5cd3a4ae64c6bd48e5d8fce185632c646676
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-11 05:55:48 +0200

    Add sign-in sign-out pages

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '@/components/auth/login/login-form';
+
+export default function SignInPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+}
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from '@/components/auth/register/register-form';
+
+export default function SignUpPage() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <RegisterForm />
+    </div>
+  );
+}
