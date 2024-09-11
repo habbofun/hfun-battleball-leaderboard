1e60976e9c446a74e9bfe255b8657af1d156a9c8
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-11 07:09:08 +0200

    Added some token utils

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -1,6 +1,15 @@
+import { alphabet, generateRandomString } from 'oslo/crypto';
+
 export const hashSettings = {
   memoryCost: 19456,
   timeCost: 2,
   outputLen: 32,
   parallelism: 1,
+};
+
+export const TOKEN_LENGTH = 32;
+export const TOKEN_TTL = 1000 * 60 * 5; // 5 min
+
+export const generateRandomToken = async (length: number) => {
+  return generateRandomString(length, alphabet('0-9', 'a-z', 'A-Z'));
 };
