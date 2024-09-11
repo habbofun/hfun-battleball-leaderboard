25a969d756d21724f8eb19a01d85ee10296af8bd
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-11 05:56:26 +0200

    Add lucia server libs

---
+++ src/server/lucia.ts
@@ -0,0 +1,40 @@
+import { PrismaAdapter } from '@lucia-auth/adapter-prisma';
+import { Role } from '@prisma/client';
+import { Lucia } from 'lucia';
+
+import db from '@/lib/db';
+
+const adapter = new PrismaAdapter(db.session, db.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    name: 'hfun-battleball-leaderboard-session',
+    expires: true,
+    attributes: {
+      // set to `true` when using HTTPS
+      secure: process.env.NODE_ENV === 'production',
+    },
+  },
+  getUserAttributes: (attributes) => {
+    return {
+      username: attributes.username,
+      email: attributes.email,
+      role: attributes.role,
+      emailVerified: attributes.emailVerified,
+    };
+  },
+});
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  username: string;
+  email: string;
+  role: Role;
+  emailVerified: boolean;
+}
