5e017a3309b10a67d90f38005952443082d029c6
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-14 17:38:34 +0200

    Removed word staff from the group name

--- src/components/habbo/staff/staff-list.tsx
+++ src/components/habbo/staff/staff-list.tsx
@@ -79,7 +79,7 @@ export function StaffList() {
                   ) : (
                     <StarIcon className="mr-2 text-gray-400 w-6 h-6" />
                   )}
-                  {group} Staff
+                  {group}
                 </h2>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                   {groupedStaff[group].map((member) => (
