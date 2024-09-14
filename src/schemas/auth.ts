a42603cc595459a6b4232a33d551137ff65ab720
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-14 09:23:38 +0200

    Use properly zod schema object

--- src/schemas/auth.ts
+++ src/schemas/auth.ts
@@ -31,18 +31,17 @@ export const loginSchema = object({
 
 export type LoginSchema = z.infer<typeof loginSchema>;
 
-export const forgotPasswordSchema = z
-  .object({
-    email: string({ required_error: 'Email is required' })
-      .min(1, 'Email is required')
-      .email('Invalid email'),
-    newPassword: z
-      .string()
-      .min(6, 'Password must be at least 6 characters long'),
-    confirmPassword: z
-      .string()
-      .min(6, 'Password must be at least 6 characters long'),
-  })
+export const forgotPasswordSchema = object({
+  email: string({ required_error: 'Email is required' })
+    .min(1, 'Email is required')
+    .email('Invalid email'),
+  newPassword: z
+    .string()
+    .min(6, 'Password must be at least 6 characters long'),
+  confirmPassword: z
+    .string()
+    .min(6, 'Password must be at least 6 characters long'),
+})
   .refine((data) => data.newPassword === data.confirmPassword, {
     message: "Passwords don't match",
     path: ['confirmPassword'],
