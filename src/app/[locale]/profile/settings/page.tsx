0ebec7a95722f39774ce935295f982c4a3deb0f1
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-14 09:42:36 +0200

    Remove profile settings page till making 2fa flow

--- src/app/[locale]/profile/settings/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ProfileSettings } from '@/components/auth/profile/settings';
-
-export default function ProfileSettingsPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <ProfileSettings />
-    </div>
-  );
-}
