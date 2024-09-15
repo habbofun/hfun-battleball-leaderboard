96cf361f68eb83daba720eac68a42c4a1e4dbe4c
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-15 23:44:28 +0200

    Added disable two factor action

--- src/server/actions/auth/two-factor/two-factor.ts
+++ src/server/actions/auth/two-factor/two-factor.ts
@@ -32,15 +32,50 @@ export const generatetwoFactorTokenAndUri = async (sessionId: string) => {
     const uri = createTOTPKeyURI('hfun.info', user.email, twoFactorToken);
     const qrCode = await QRCode.toDataURL(uri);
 
+    // Extract the secret from the URI
+    const secret = new URL(uri).searchParams.get('secret') || '';
+
     return {
       success: true,
       uri: uri,
+      secret: secret,
       qrCode: qrCode,
     };
   } catch (error) {
     return {
       success: false,
       error: 'Failed to generate two factor secret and URI',
+    };
+  }
+};
+
+export const disableTwoFactor = async (sessionId: string) => {
+  try {
+    const { user } = await lucia.validateSession(sessionId);
+
+    if (!user) {
+      return {
+        success: false,
+        error: 'User not found',
+      };
+    }
+
+    await db.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        twoFactorToken: null,
+      },
+    });
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: 'Failed to disable two factor',
     };
   }
 };
