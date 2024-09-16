79fd2df9d0493ddc441b2038924ca523e1cd7ce5
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-16 02:47:15 +0200

    Removed generateHabboToken func from utils lib

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,13 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-
-export function generateHabboToken(length: number = 6): string {
-  const characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
