9eb6e11cba03d036589a1b4cbcb974c05ccb0275
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-17 22:37:19 +0200

    Fix staff page flex

--- src/app/[locale]/(hfun)/staff/page.tsx
+++ src/app/[locale]/(hfun)/staff/page.tsx
@@ -9,7 +9,7 @@ export default async function StaffPage() {
   const isAdmin = user?.role === 'admin';
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col flex-1">
       <main className="flex-grow container mx-auto px-4 py-8 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold mb-8 text-center">Habbo Staff</h1>
         <Suspense
