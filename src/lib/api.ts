8a77f843b700072b1be94098a9b759a0da8d875a
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-18 09:10:09 +0200

    Added localApiClient to api lib

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const leaderboardApiClient = axios.create({
   baseURL: 'https://leaderboard.hfun.info',
 });
+
+export const localApiClient = axios.create({
+  baseURL: 'http://localhost:3000',
+});
