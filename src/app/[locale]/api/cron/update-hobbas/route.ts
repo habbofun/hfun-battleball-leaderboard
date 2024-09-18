8887f6e72f48e50b26a07a441f22c6cd14a2f6c5
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-18 06:52:52 +0200

    Create hobba cronjob route

---
+++ src/app/[locale]/api/cron/update-hobbas/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from 'next/server';
+
+import { updateAllHobbaData } from '@/server/actions/habbo/hobba/manage-hobba';
+
+export async function GET(request: Request) {
+  const authHeader = request.headers.get('authorization');
+
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+
+  try {
+    const result = await updateAllHobbaData();
+
+    console.log('Hobba data updated:', result);
+    return NextResponse.json(result);
+  } catch (error) {
+    console.error('Error updating hobba data:', error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 },
+    );
+  }
+}
+
+export const config = {
+  maxDuration: 300, // 5 minutes
+};
