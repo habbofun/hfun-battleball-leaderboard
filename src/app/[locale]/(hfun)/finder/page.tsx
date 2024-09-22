4f1cf6fcbe3f29c7aa1a66bae83b28ba6bbb8022
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-22 22:26:18 +0200

    Changed from main to div tags

--- src/app/[locale]/(hfun)/finder/page.tsx
+++ src/app/[locale]/(hfun)/finder/page.tsx
@@ -3,9 +3,9 @@ import { HabboUserInfo } from '@/components/habbo/finder/habbo-user-info';
 export default function FinderPage() {
   return (
     <div className="flex flex-col flex-1 bg-background text-foreground">
-      <main className="flex-grow flex flex-col justify-center items-center p-4">
+      <div className="flex-grow flex flex-col justify-center items-center p-4">
         <HabboUserInfo />
-      </main>
+      </div>
     </div>
   );
 }
