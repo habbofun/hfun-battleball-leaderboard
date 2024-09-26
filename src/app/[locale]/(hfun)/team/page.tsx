afac5634d02b2fa8506c2d5da534ecbae305677e
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-26 19:12:45 +0200

    Fix typo

--- src/app/[locale]/(hfun)/team/page.tsx
+++ src/app/[locale]/(hfun)/team/page.tsx
@@ -16,7 +16,7 @@ export default async function TeamPage() {
     <div className="flex flex-col flex-1">
       <main className="container mx-auto px-4 py-8 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center mb-8">
-          <h1 className="text-3xl font-bold">Team Member</h1>
+          <h1 className="text-3xl font-bold">Team Members</h1>
           {session && isAdmin && (
             <Link href="/team/admin">
               <Button>Admin</Button>
