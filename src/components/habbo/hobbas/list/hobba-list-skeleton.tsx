207c09b5df58b0bce5c2207ef2aeef9305445928
kWAY <55437151+kWAYTV@users.noreply.github.com>
2024-09-26 18:27:39 +0200

    Adjust hobbas skeleton to match the appearing content

--- src/components/habbo/hobbas/list/hobba-list-skeleton.tsx
+++ src/components/habbo/hobbas/list/hobba-list-skeleton.tsx
@@ -1,23 +1,23 @@
-import { Card, CardContent } from '@/components/ui/card';
+import { Card } from '@/components/ui/card';
 import { Skeleton } from '@/components/ui/skeleton';
 
 export const HobbaListSkeleton = () => (
   <div className="space-y-8">
     {[...Array(2)].map((_, groupIndex) => (
       <div key={groupIndex}>
-        <Skeleton className="h-6 w-32 mb-4" />
+        <Skeleton className="h-6 w-32 mb-4" /> {/* Group title */}
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
           {[...Array(6)].map((_, cardIndex) => (
-            <Card key={cardIndex}>
-              <CardContent className="flex flex-col items-center p-4 space-y-2">
-                <Skeleton className="h-4 w-3/4" />
-                <Skeleton className="h-24 w-24 rounded-full" />
-                <Skeleton className="h-4 w-16" />
-                <div className="w-full space-y-1 mt-2">
-                  <Skeleton className="h-3 w-full" />
-                  <Skeleton className="h-3 w-full" />
-                </div>
-              </CardContent>
+            <Card key={cardIndex} className="p-4 flex flex-col items-center">
+              <Skeleton className="h-5 w-3/4 mb-4" /> {/* Hobba name */}
+              <Skeleton className="h-24 w-24 rounded-full mb-4" />{' '}
+              {/* Hobba image */}
+              <Skeleton className="h-12 w-full mb-4" /> {/* Motto */}
+              <Skeleton className="h-4 w-16 mb-4" /> {/* Badge */}
+              <div className="w-full space-y-1 mt-auto">
+                <Skeleton className="h-3 w-full" /> {/* Last seen */}
+                <Skeleton className="h-3 w-full" /> {/* Registered */}
+              </div>
             </Card>
           ))}
         </div>
